0442a2245ae7f33b5e622f020c6afb6729ed41d9
codingsapienss <contactprsant@gmail.com>
2025-11-22 01:15:40 +0530

    refactored the create and update function of PlayScene

--- src/scenes/PlayScene.js
+++ src/scenes/PlayScene.js
@@ -26,33 +26,50 @@ class PlayScene extends Phaser.Scene {
 
 
     create() {
+        this.createBG()
+        this.createBird()
+        this.createPipes()
+        this.handleInputs()
+    }
+
+    update() {
+        this.checkGameStatus()
+        this.recyclePipes()
+    }
+
+    createBG() {
         this.add.image(0, 0, "sky-bg").setOrigin(0, 0);
+
+    }
+
+    createBird() {
         this.bird = this.physics.add.sprite(this.config.startPosition.x, this.config.startPosition.y, "bird").setOrigin(0);
         this.bird.body.gravity.y = 400
+    }
 
+    createPipes() {
         this.pipes = this.physics.add.group()
-
 
         for (let i = 0; i < PIPES_TO_RENDER; i++) {
             const upperPipe = this.pipes.create(0, 0, "pipe").setOrigin(0, 1);
 
             const lowerPipe = this.pipes.create(0, 0, "pipe").setOrigin(0);
 
-
             this.placePipe(upperPipe, lowerPipe)
         }
 
         this.pipes.setVelocityX(-this.velocity)
+    }
 
+    handleInputs() {
         this.input.on("pointerdown", this.flap, this)
         this.input.keyboard.on("keydown-SPACE", this.flap, this)
     }
 
-    update() {
+    checkGameStatus() {
         if (this.bird.y > this.config.height || this.bird.y < -this.bird.height) {
             this.restartPlayerPosition()
         }
-        this.recyclePipes()
     }
 
     placePipe(uPipe, lPipe) {
